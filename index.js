dfc6d676128e94b40ebecaaf6b13bd94db92d28a
chakshu gautam <chakshu@samagragovernance.in>
2019-05-14 20:25:09 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require('express')
 const port = process.env.PORT || 3000
+const puppeteer = require('puppeteer')
+
 const app = express()
+app.set('view engine', 'html')
 
-app.get('/', (req, res) => res.redirect('/images/png'))
-
-app.use('/images', require('./routes/images'))
+app.get('/export/pdf', (req, res) => {
+  (async () => {
+      console.log(req.query);
+      const browser = await puppeteer.launch()
+      const page = await browser.newPage()
+      page.setViewport({width: 1400, height: 1000});
+      await page.goto(req.query.url, {"waitUntil" : "networkidle0"});
+      
+      const buffer = await page.pdf({
+        format: 'A4', 
+        landscape: true, 
+        printBackground: true,
+        displayHeaderFooter: true
+      });
+      res.type('application/pdf')
+      res.send(buffer)
+      browser.close()
+  })()
+});
 
 app.listen(port, function() {
   console.log('Server listening at http://localhost:' + port)
-})
+});
