6a72fabb27c33410d891efdf7e305d2dbfbe763b
chakshu gautam <chakshu@samagragovernance.in>
2019-06-13 14:40:45 +0530

    Priniting back to normal

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ var cors = require('cors')
 const app = express()
 app.use(cors())
 app.set('view engine', 'html')
+let browser;
 
 app.get('/export/pdf', (req, res) => {
   (async () => {
       console.log(req.query);
-      const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
       const page = await browser.newPage()
-      page.setViewport({width: 1400, height: 1000});
+      page.setViewport({width: 1400, height: 10000});
       await page.goto(req.query.url, {"waitUntil" : "networkidle0"});
       
       const buffer = await page.pdf({
@@ -21,12 +21,17 @@ app.get('/export/pdf', (req, res) => {
         printBackground: true,
         displayHeaderFooter: true
       });
-      res.type('application/pdf')
-      res.send(buffer)
-      browser.close()
+      await page.close();
+      res.type('application/pdf');
+      res.send(buffer);
   })()
 });
 
-app.listen(port, "0.0.0.0", function() {
-  console.log('Server listening at http://localhost:' + port)
-});
+(async () => {
+  browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+  app.listen(port, "0.0.0.0", function() {
+    console.log('Server listening at http://localhost:' + port)
+  });
+})();
+
+
