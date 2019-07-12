bfa3e5afb7b7f133972bd120778783140b7442db
chakshu gautam <chakshu@samagragovernance.in>
2019-07-12 13:13:33 +0530

    fix: regex for url validation

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.set('view engine', 'html')
 let browser;
 
 function validateUrl(value) {
-  return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
+  return /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi.test(value);
 }
 
 app.get('/export/pdf', (req, res) => {
@@ -31,7 +31,8 @@ app.get('/export/pdf', (req, res) => {
           res.type('application/pdf');
           res.send(buffer);
         }catch(e){
-          res.send({status: "Couldn't complete download",url:req.query.url });
+          console.log(e);
+          res.send({status: "Couldn't complete download", url:req.query.url });
         }
       }else{
         res.send({status: "Malformed URL", url:req.query.url});
